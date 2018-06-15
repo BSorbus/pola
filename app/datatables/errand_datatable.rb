5e4861a449574f28e8b030a4746ab906539b9d4e
BSorbus <BSorbus@gmail.com>
2018-06-15 10:15:29 +0200

    Add attachments_count to Errand index

--- app/datatables/errand_datatable.rb
+++ app/datatables/errand_datatable.rb
@@ -4,14 +4,15 @@ class ErrandDatatable < AjaxDatatablesRails::Base
 
   def view_columns
     @view_columns ||= {
-      id:             { source: "Errand.id", cond: :eq, searchable: false, orderable: false },
-      number:         { source: "Errand.number", cond: :like, searchable: true, orderable: true },
-      order_date:     { source: "Errand.order_date", cond: :like, searchable: true, orderable: true },
-      principal:      { source: "Errand.principal",  cond: :like, searchable: true, orderable: true },
-      adoption_date:  { source: "Errand.adoption_date", cond: :like, searchable: true, orderable: true },
-      start_date:     { source: "Errand.start_date", cond: :like, searchable: true, orderable: true },
-      end_date:       { source: "Errand.end_date", cond: :like, searchable: true, orderable: true },
-      status:         { source: "ErrandStatus.name", cond: :like, searchable: true, orderable: true }
+      id:                { source: "Errand.id", cond: :eq, searchable: false, orderable: false },
+      number:            { source: "Errand.number", cond: :like, searchable: true, orderable: true },
+      order_date:        { source: "Errand.order_date", cond: :like, searchable: true, orderable: true },
+      principal:         { source: "Errand.principal",  cond: :like, searchable: true, orderable: true },
+      adoption_date:     { source: "Errand.adoption_date", cond: :like, searchable: true, orderable: true },
+      start_date:        { source: "Errand.start_date", cond: :like, searchable: true, orderable: true },
+      end_date:          { source: "Errand.end_date", cond: :like, searchable: true, orderable: true },
+      status:            { source: "ErrandStatus.name", cond: :like, searchable: true, orderable: true },
+      attachments_count: { source: "Errand.attachments_count", cond: :like, searchable: true, orderable: true }
     }
   end
 
@@ -25,7 +26,8 @@ class ErrandDatatable < AjaxDatatablesRails::Base
         adoption_date:  record.adoption_date.present? ? record.adoption_date.strftime("%Y-%m-%d") : '' ,
         start_date:     record.start_date.present? ? record.start_date.strftime("%Y-%m-%d") : '' ,
         end_date:       record.end_date.present? ? record.end_date.strftime("%Y-%m-%d") : '' ,
-        status:         record.errand_status.try(:name)
+        status:         record.errand_status.try(:name),
+        attachments_count: badge(record).html_safe
       }
     end
   end
@@ -36,6 +38,14 @@ class ErrandDatatable < AjaxDatatablesRails::Base
     Errand.joins(:errand_status).references(:errand_status).all
   end
 
+  def badge(rec)
+    count = rec.try(:attachments_count)
+    if count > 0
+      "<div style='text-align: center'><span class='badge alert-info'>" + "#{count}" + "</span></div>"
+    else
+      "<div style='text-align: center'><span class='badge'>" + "#{count}" + "</span></div>"
+    end
+  end
 
   # ==== These methods represent the basic operations to perform on records
   # and feel free to override them
